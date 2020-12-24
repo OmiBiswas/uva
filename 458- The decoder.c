510cab669c5decab31e8ee25c3a4796ac0edf69a
Omi <75533083+OmiBiswas@users.noreply.github.com>
2020-12-24 14:29:16 +0600

    Create 458- The decoder.c

---
+++ 458- The decoder.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    while(scanf("%c",&ch)==1)
+    {
+        if(ch=='\n')
+            printf("\n");
+        else
+        {
+         ch=ch-7;
+         printf("%c",ch);
+        }
+        }
+        return 0;
+    }
